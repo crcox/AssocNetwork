21a53a198e355751cee44052b780da3cd8c936ab
Stan West <swest19@lsu.edu>
2020-09-16 00:05:03 -0500

    This is the completed code for creating the network

---
+++ AssocNetwork_Save.R
@@ -0,0 +1,57 @@
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Load Package:              'Ctrl + Shift + L'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' Derive sorted set from a vector of input
+#' 
+#' @param x A vector
+#' @return Unique values of x in ascending order
+sortedSet <- function(x) {
+  return(sort(unique(x)))
+}
+
+
+#' Identify which elements of x are cues
+#' 
+#' @param x A vector
+#' @param cues A vector defining the full set of unique cues
+#' @return A logical vector with \code{length(x)} elements.
+iscue <- function(x, cues) { 
+  return(x %in% cues)
+}
+
+#' Filter cue-response  
+#' @param d A data frame with two columns: "cue" and "resp".
+#' @return A subset of rows from the original data frame.
+filterNonCueResponses <- function(d){
+  return(subset(d, iscue(d$resp, cues = sortedSet(d$cue))))
+}
+
+#' Generate a logical response profile
+#' 
+#' @param resp A vector containing responses associated with a cue
+#' @param all_unique A vector containing the full set of unique values that define the profile.
+#' @return A logical vector with \code{length(all_uniq)} elements.
+respProfile <- function(resp, all_unique) {
+
+  return(all_unique %in% resp)
+}
+
+#' Generate an unweighted associative network
+#' 
+#' @param d A data frame with two columns: "cue" and "resp".
+#' @return A logical matrix with dimensions \code{length(unique(d$cue))} by \code{length(unique(d$cue))}.
+assocNetwork <- function(d) {
+  d <- filterNonCueResponses(d)
+  cue_set <- sortedSet(d$cue)
+  x <- tapply(d$resp,
+              INDEX = d$cue,
+              FUN = respProfile,
+              all_unique = cue_set)
+  net <- do.call(rbind, x)
+  colnames(net) <- cue_set
+  return(net)
+}
+
+
